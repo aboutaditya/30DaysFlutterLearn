d7146167e72b93a4dfb8a010250b4ac33fa87d64
Aditya Thakur <itsadityathakur001@gmail.com>
2022-06-06 13:48:32 +0530

    Day5 complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/home_page.dart';
 import 'package:flutter_application_1/pages/login_page.dart';
+import 'package:flutter_application_1/utils/Routes.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(Myapp());
+  runApp(const Myapp());
 }
 
 class Myapp extends StatelessWidget {
+  const Myapp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,14 +19,15 @@ class Myapp extends StatelessWidget {
         primarySwatch: Colors.deepPurple,
         fontFamily: GoogleFonts.lato().fontFamily,
       ),
+      debugShowCheckedModeBanner: false,
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
       // initialRoute: "/home",
       routes: {
-        "/": (context) => LoginPage(),
-        "/home": (context) => Home(),
-        "/Login": (context) => LoginPage()
+        "/": (context) => const LoginPage(),
+        MyRoutes.homeRoute: (context) => const Home(),
+        MyRoutes.loginRoute: (context) => const LoginPage()
       },
     );
   }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     int days = 30;
