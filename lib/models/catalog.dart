c5c24b27c915fbb29fb2aa967f6b371fb1d0f4be
Aditya Thakur <itsadityathakur001@gmail.com>
2022-06-08 17:12:54 +0530

    day 9 complete

---
+++ lib/models/catalog.dart
@@ -0,0 +1,9 @@
+class Item {
+  final String name;
+  final String id;
+  final String desc;
+  final num price;
+  final String iurl;
+
+  Item(this.name, this.id, this.desc, this.price, this.iurl);
+}
